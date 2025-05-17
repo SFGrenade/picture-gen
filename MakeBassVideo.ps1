4860b204fe89751f2460163f6a29feb9a93f5348
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-05-17 02:41:14 +0200

    make it wobble use better low+highpass - also output filtered audio add logging messages that can be used for timing - like a 2:14min video taking 12 minutes to render

--- MakeBassVideo.ps1
+++ MakeBassVideo.ps1
@@ -1,20 +1,47 @@
 $fps = 60
 $half_fps = 30
 
+function Log-Message
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$LogMessage
+    )
+
+    Write-Output("{0} - {1}" -f (Get-Date), $LogMessage) >>../__internal_timing.log
+}
+Write-Output("") >../__internal_timing.log
+
+Log-Message("Getting duration and bitrate of audio.wav...")
+
 $time = $(ffprobe -select_streams a:0 -show_entries "stream=duration" -of compact audio.wav 2>$null) -replace '^[^=]+=(.*)$','$1';
 $bitrate = $(ffprobe -select_streams a:0 -show_entries "stream=bit_rate" -of compact audio.wav 2>$null) -replace '^[^=]+=(.*)$','$1';
 
+Log-Message("Got duration and bitrate of audio.wav: $time s, $bitrate b/s")
+
+Log-Message("Preprocessing audio.wav...")
+
 ffmpeg -hide_banner -y -hwaccel cuda -hwaccel_output_format cuda `
     -r $fps -max_size "$bitrate / 8 / $fps" -i audio.wav -shortest `
-    -filter_complex "[0:a] lowpass=frequency=80 : poles=2 : width_type=q : width=0.707 : precision=auto, astats=length=1/$fps : metadata=1 : reset=1 : measure_perchannel=none : measure_overall=Peak_level, ametadata=print : key=lavfi.astats.Overall.Peak_level : file='../_internal_levels.log' ;" `
-    -f null `
-- #>_audio_analysis.log 2>&1
+    -filter_complex "[0:a] firequalizer=gain_entry='entry(19, -INF); entry(20, 0); entry(100, 0); entry(101, -INF)' : zero_phase=on, astats=length=1/$fps : metadata=1 : reset=1 : measure_perchannel=none : measure_overall=Min_level, astats=length=1/$fps : metadata=1 : reset=1 : measure_perchannel=none : measure_overall=Max_level, astats=length=1/$fps : metadata=1 : reset=1 : measure_perchannel=none : measure_overall=Peak_level, astats=length=1/$fps : metadata=1 : reset=1 : measure_perchannel=none : measure_overall=RMS_level, ametadata=mode=print : file='../_internal_levels.log' [TO_OUT];" `
+    -map '[TO_OUT]' `
+__filtered.wav #>_audio_analysis.log 2>&1
 
-python ../RmsLogParser.py #>../_python_script.log 2>&1
+Log-Message("Preprocessed audio.wav")
+
+Log-Message("Parsing audio stats...")
+
+python ../RmsLogParser.py "lavfi.astats.Overall.Max_level" #>../_python_script.log 2>&1
+
+Log-Message("Parsed audio stats")
+
+Log-Message("Rendering final video...")
 
 ffmpeg -hide_banner -y -hwaccel cuda -hwaccel_output_format cuda `
     -loop 1 -r $fps -i ..\bg.old.png -r $fps -max_size "$bitrate / 8 / $fps" -i audio.wav -loop 1 -r $fps -i ..\circle.png -loop 1 -r $fps -i art.png -shortest `
-    -filter_complex "[0:v] format=rgba, scale=flags=16388 : width=1920 : height=1080 [BG_IN]; [2:v] format=rgba, sendcmd=filename='../_internal_ball_scale.log', scale@circle=flags=16388 : width=in_w : height=in_h [CIRCLE_IN]; [3:v] format=rgba, scale=flags=16388 : width=917 : height=812 [ART_IN]; [1:a] showwaves=size=917x387 : rate=$fps : mode=p2p : draw=full : colors=#FF0000FF|#AA0000FF, format=rgba, dilation, scale=flags=16388 : width=917 : height=387 [waves]; [1:a] showfreqs=size=917x387 : rate=$fps : fscale=log : win_size=4096 : win_func=flattop : colors=#600000FF|#400000FF, format=rgba, scale=flags=16388 : width=917 : height=387 [freqs]; [BG_IN][freqs] overlay=format=auto : alpha=straight : eval=init : x=979 : y=24, drawtext=fontfile=C\\:/Windows/Fonts/NotoSans-Regular.ttf : fontsize=60 : textfile=title.txt : fix_bounds=true : fontcolor=#FFFFFFFF : x=1437-(text_w/2) : y=217-(text_h/2) [bg+freqs+title]; [bg+freqs+title][waves] overlay=format=auto : alpha=straight : eval=init : x=979 : y=449 [bg+title+waves]; [bg+title+waves][ART_IN] overlay=format=auto : alpha=straight : eval=init : x=24 : y=24 [bg+title+waves+art]; [bg+title+waves+art][CIRCLE_IN] overlay=format=auto : alpha=straight : eval=frame : y=964.5-(overlay_h/2) : x=114.5+((1804.5-114.5)*(t/$time))-(overlay_w/2) [ready-for-output]; [ready-for-output] yadif, format=pix_fmts=yuv420p : color_spaces=bt709 [VIDEO_OUT]" `
+    -filter_complex "nullsrc=size=1920x1080 : rate=$fps, sendcmd=filename='../_internal_sendcmd_cmds_dx.log', lut@dx=r='random(1)*255':g='random(1)*255':b='random(1)*255' [DISPLACE_1_IN]; nullsrc=size=1920x1080 : rate=$fps, sendcmd=filename='../_internal_sendcmd_cmds_dy.log', lut@dy=r='random(1)*255':g='random(1)*255':b='random(1)*255' [DISPLACE_2_IN]; [0:v] format=rgba, scale=flags=16388 : width=1920 : height=1080 [BG_IN]; [2:v] format=rgba, sendcmd=filename='../_internal_sendcmd_cmds_b.log', scale@b=flags=16388 : width=in_w : height=in_h [CIRCLE_IN]; [3:v] format=rgba, scale=flags=16388 : width=917 : height=812 [ART_IN]; [1:a] showwaves=size=917x387 : rate=$fps : mode=p2p : draw=full : colors=#FF0000FF|#AA0000FF, format=rgba, dilation, scale=flags=16388 : width=917 : height=387 [waves]; [1:a] showfreqs=size=917x387 : rate=$fps : fscale=log : win_size=4096 : win_func=flattop : colors=#600000FF|#400000FF, format=rgba, scale=flags=16388 : width=917 : height=387 [freqs]; [BG_IN][freqs] overlay=format=auto : alpha=straight : eval=init : x=979 : y=24, drawtext=fontfile=C\\:/Windows/Fonts/NotoSans-Regular.ttf : fontsize=60 : textfile=title.txt : fix_bounds=true : fontcolor=#FFFFFFFF : x=1437-(text_w/2) : y=217-(text_h/2) [bg+freqs+title]; [bg+freqs+title][waves] overlay=format=auto : alpha=straight : eval=init : x=979 : y=449 [bg+title+waves]; [bg+title+waves][ART_IN] overlay=format=auto : alpha=straight : eval=init : x=24 : y=24 [bg+title+waves+art]; [bg+title+waves+art][CIRCLE_IN] overlay=format=auto : alpha=straight : eval=frame : y=964.5-(overlay_h/2) : x=114.5+((1804.5-114.5)*(t/$time))-(overlay_w/2) [ready-for-output]; [ready-for-output] [DISPLACE_1_IN][DISPLACE_2_IN] displace=edge=wrap, yadif, format=pix_fmts=yuv420p : color_spaces=bt709 [VIDEO_OUT]" `
     -c:a aac -ac 2 -ar 48000 `
         -aac_coder fast -b:a 512K `
     -c:v libx264 `
@@ -28,4 +55,6 @@ ffmpeg -hide_banner -y -hwaccel cuda -hwaccel_output_format cuda `
     -strict very `
     -map "1:a" `
     -map "[VIDEO_OUT]" `
-z_bass.mp4 #>_video_rendering.log 2>&1
+youtube.mp4 #>_video_rendering.log 2>&1
+
+Log-Message("Rendered final video")
--- RmsLogParser.py
+++ RmsLogParser.py
@@ -1,57 +1,114 @@
 from dataclasses import dataclass
 import math
 import os
+import random
 import sys
 
 @dataclass
 class TimeLevelPoint:
     time_point_from: float = 0.0
-    time_point_to: float = 0.0
     level_value: float = 0.0
 
+def normalToBallZoom(normal: float) -> float:
+    normal = max(0.0, min(1.0, normal))
+    return 0.5 + (normal * 0.5)
+
+def normalToDisplaceRandom(normal: float) -> float:
+    normal = max(0.0, min(1.0, normal))
+    random_value: float = random.uniform(-0.5, 0.5)
+    make_it_converge: float = math.pow(normal, 2)  # values towards 0 are more likely, instead of it being linear
+    scale: float = 0.25
+    return 0.5 + (random_value * make_it_converge * scale)
+
+def getNormal(value: float, min_val: float, max_val: float) -> float:
+    return (value - min_val) / (max_val - min_val)
+
 def main() -> None:
-    listOfPoints: list[TimeLevelPoint] = []
-    listOfValues: list[float] = []
+    dictOfListOfPoints: dict[str, list[TimeLevelPoint]] = {}
+    dictOfListOfValues: dict[str, list[float]] = {}
+
     time_point: float = 0.0
     level_value: float = 0.0
+    number_time_points: int = 0
     with open(os.path.join("..", "_internal_levels.log")) as fin:
         for line in fin:
             if "pts_time:" in line:
                 parts: list[str] = line.split()
                 time_point = float(parts[2].split(":")[1])
-                if len(listOfPoints) > 0:
-                    listOfPoints[-1].time_point_to = time_point
-            elif "lavfi.astats.Overall.Peak_level" in line:
-                parts: list[str] = line.split("=")
-                level_value = float(parts[-1])
-                if math.isinf(level_value) and level_value < 0:
-                    # negative infinity, presumably no bass, so hardcode -120.0 for now
-                    level_value = -120.0
-                listOfValues.append(level_value)
-                listOfPoints.append(TimeLevelPoint(time_point, -1, level_value))
-
-    minVal: float = min(listOfValues)
-    maxVal: float = max(listOfValues)
-    #print("minVal:", minVal)
-    #print("maxVal:", maxVal)
-
-    with open(os.path.join("..", "_internal_ball_scale.log"), "w") as fout:
-        for point in listOfPoints:
-            relative: float = (point.level_value - minVal) / (maxVal - minVal)
-            #print("relative:", relative)
-            zoom: float = 0.5 + (relative * 0.5)
-
-            tpf_str: str = f"{point.time_point_from:.3f}"
-            tpt_str: str = f"{point.time_point_to:.3f}"
-            zoom_str: str = f"{zoom:.3f}"
-            #tpf_str: str = f"{point.time_point_from}"
-            #tpt_str: str = f"{point.time_point_to}"
-            #zoom_str: str = f"{zoom}"
-
-            if point.time_point_to >= 0:
-                fout.write(f"{tpf_str} scale@circle width '{zoom_str}*in_w', scale@circle height '{zoom_str}*in_h';\n")
+                number_time_points += 1
             else:
-                fout.write(f"{tpf_str} scale@circle width '{zoom_str}*in_w', scale@circle height '{zoom_str}*in_h';\n")
+                parts: list[str] = line.split("=")
+
+                lineName: str = parts[0]
+                if lineName not in dictOfListOfPoints:
+                    dictOfListOfPoints[lineName] = []
+                if lineName not in dictOfListOfValues:
+                    dictOfListOfValues[lineName] = []
+
+                level_value: float = float(parts[-1])
+                dictOfListOfPoints[lineName].append(TimeLevelPoint(time_point, level_value))
+                dictOfListOfValues[lineName].append(level_value)
+
+    minVals: dict[str, float] = {}
+    maxVals: dict[str, float] = {}
+    for x in dictOfListOfValues:
+        minVals[x] = min(dictOfListOfValues[x])
+        maxVals[x] = max(dictOfListOfValues[x])
+        #print(f"minVal for {x}: {minVal}")
+        #print(f"maxVal for {x}: {maxVal}")
+
+    with open(os.path.join("..", "_internal_sendcmd_cmds.log"), "w") as fout, open(os.path.join("..", "_internal_sendcmd_cmds_b.log"), "w") as fo_b, open(os.path.join("..", "_internal_sendcmd_cmds_dx.log"), "w") as fo_dx, open(os.path.join("..", "_internal_sendcmd_cmds_dy.log"), "w") as fo_dy:
+        for i in range(number_time_points):
+            point_min_level: TimeLevelPoint = dictOfListOfPoints["lavfi.astats.Overall.Min_level"][i]
+            point_max_level: TimeLevelPoint = dictOfListOfPoints["lavfi.astats.Overall.Max_level"][i]
+            point_peak_level: TimeLevelPoint = dictOfListOfPoints["lavfi.astats.Overall.Peak_level"][i]
+            point_rms_level: TimeLevelPoint = dictOfListOfPoints["lavfi.astats.Overall.RMS_level"][i]
+
+            relative_min_level: float = getNormal(point_min_level.level_value, minVals["lavfi.astats.Overall.Min_level"], maxVals["lavfi.astats.Overall.Min_level"])
+            relative_max_level: float = getNormal(point_max_level.level_value, minVals["lavfi.astats.Overall.Max_level"], maxVals["lavfi.astats.Overall.Max_level"])
+            relative_peak_level: float = getNormal(point_peak_level.level_value, minVals["lavfi.astats.Overall.Peak_level"], maxVals["lavfi.astats.Overall.Peak_level"])
+            relative_rms_level: float = getNormal(point_rms_level.level_value, minVals["lavfi.astats.Overall.RMS_level"], maxVals["lavfi.astats.Overall.RMS_level"])
+
+            ball_zoom: float = normalToBallZoom(relative_peak_level)
+            displace_value_x_r: float = normalToDisplaceRandom(relative_max_level)
+            displace_value_x_g: float = normalToDisplaceRandom(relative_max_level)
+            displace_value_x_b: float = normalToDisplaceRandom(relative_max_level)
+            displace_value_y_r: float = normalToDisplaceRandom(relative_max_level)
+            displace_value_y_g: float = normalToDisplaceRandom(relative_max_level)
+            displace_value_y_b: float = normalToDisplaceRandom(relative_max_level)
+
+            #tpf_str: str = f"{point_max_level.time_point_from:.3f}"
+            ball_zoom_str: str = f"{ball_zoom:.3f}"
+            displace_value_x_r_str: str = f"{displace_value_x_r:.3f}"
+            displace_value_x_g_str: str = f"{displace_value_x_g:.3f}"
+            displace_value_x_b_str: str = f"{displace_value_x_b:.3f}"
+            displace_value_y_r_str: str = f"{displace_value_y_r:.3f}"
+            displace_value_y_g_str: str = f"{displace_value_y_g:.3f}"
+            displace_value_y_b_str: str = f"{displace_value_y_b:.3f}"
+            tpf_str: str = f"{point_max_level.time_point_from}"
+            #ball_zoom_str: str = f"{ball_zoom}"
+            #displace_value_x_r_str: str = f"{displace_value_x_r}"
+            #displace_value_x_g_str: str = f"{displace_value_x_g}"
+            #displace_value_x_b_str: str = f"{displace_value_x_b}"
+            #displace_value_y_r_str: str = f"{displace_value_y_r}"
+            #displace_value_y_g_str: str = f"{displace_value_y_g}"
+            #displace_value_y_b_str: str = f"{displace_value_y_b}"
+
+            commands: list[str] = [
+                f"scale@b width '{ball_zoom_str}*in_w'",
+                f"scale@b height '{ball_zoom_str}*in_h'",
+                f"lut@dx r '{displace_value_x_r_str}*255'",
+                f"lut@dx g '{displace_value_x_g_str}*255'",
+                f"lut@dx b '{displace_value_x_b_str}*255'",
+                f"lut@dy r '{displace_value_y_r_str}*255'",
+                f"lut@dy g '{displace_value_y_g_str}*255'",
+                f"lut@dy b '{displace_value_y_b_str}*255'",
+            ]
+
+            fout.write(f"{tpf_str} " + ", ".join(commands) + ";\n")
+            fo_b.write(f"{tpf_str} " + ", ".join(commands[0:2]) + ";\n")
+            fo_dx.write(f"{tpf_str} " + ", ".join(commands[2:5]) + ";\n")
+            fo_dy.write(f"{tpf_str} " + ", ".join(commands[5:]) + ";\n")
 
 if __name__ == "__main__":
     main()
